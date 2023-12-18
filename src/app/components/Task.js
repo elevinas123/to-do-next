b52aacddfbba14e987ab35762017fabd002a8248
Elvinas Monstvilas <elvinas.monstvilas@gmail.com>
2023-12-18 17:33:08 +0200

    Added button to create tasks

--- src/app/components/Task.js
+++ src/app/components/Task.js
@@ -1,19 +1,27 @@
 "use client"
 import React, { useMemo, useState } from 'react';
-import Subtask from './SubTask';
+import SubTask from './SubTask';
 
 
 export default function Task(){
 
-    
+    const [subTasks, setSubTasks] = useState([])
 
     return(
-        <div className="ml-5 mt-5">
-           <div className="text-black font-semibold text-center mb-10 text-3xl">Task title</div>
-            <Subtask />
-            <Subtask />
-            <Subtask />
-            
+        <div className="ml-5 mt-5 flex flex-row">
+           <div>
+            <div className="text-black font-semibold text-center mb-10 text-3xl">Task title</div>
+                <SubTask />
+                <SubTask />
+                <SubTask />
+                
+           </div>
+            <button onClick={() => console.log("hi")} className="flex flex-row m-2">
+                    <div   className="text-gray-400 rounded-full bg-gray-200 flex justify-center items-center h-4 w-4 mt-0.5 pb-0.5">
+                        <span >+</span>
+                    </div>
+                    <div  className="ml-1 text-sm font-semibold">Add new task</div>
+            </button>
         </div>
     )
 }
--- src/app/text/page.js
+++ src/app/text/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from "../components/Navbar";
 import Task from "../components/Task";
 
@@ -11,7 +12,9 @@ export default function Text() {
             <div className="w-full flex flex-row justify-between">
                 <div className="bg-red-500 h-100vh w-0.5 ml-32"></div>
                 <Task />
+                
                 <div className="bg-red-500 h-100vh w-0.5 mr-64"></div>
+
             </div>
 
         </div>
