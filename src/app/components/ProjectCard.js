ef2f9c4470a373170979ec8099fb8e4dc0f4170d
Elvinas Monstvilas <elvinas.monstvilas@gmail.com>
2023-12-21 20:49:13 +0200

    Updatated task view progress bar to be dinamic

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -8,8 +8,19 @@ export default function ProjectCard(props) {
     const handleClick = () => {
         console.log(props)
     }
+    const getProgressBarColor = (completed, subTaskLength) => {
+        if (completed === 0) return "black";
+        if (completed === subTaskLength) return "green";
+        return completed / subTaskLength < 0.5 ? "red" : "orange";
+    };
 
-
+    const completed = props.subTasks.reduce((count, subTask) => subTask.completed ? count + 1 : count, 0)
+    const subTaskLength = props.subTasks.length
+    const progressBarStyles = {
+        backgroundColor: getProgressBarColor(completed, subTaskLength),
+        borderColor: getProgressBarColor(completed, subTaskLength),
+    }
+    console.log(progressBarStyles)
     return(
         <div onClick={handleClick} className="hover:cursor-pointer h-15vh border-2  border-gray-200  ml-3 mr-3 mt-2 rounded-lg flex flex-col p-2">
             <Link href={`/text?id=${props._id}`}>
@@ -25,10 +36,14 @@ export default function ProjectCard(props) {
             <div className="flex flex-col">
                 <div className="flex flex-row justify-between mt-3 text-sm ml-2 mr-2" >
                     <div className="text-gray-400">Progress</div>
-                    <div className="font-semibold">7/10</div>
+                    
 
                 </div>
-                <div className="border-orange-500 border-2 rounded-full mt-2 mb-2 mr-2 ml-2"></div>
+                <div className="flex flex-row">
+                    <progress className="progress w-56 m-2" value={completed/subTaskLength*100} max="100"></progress>
+                    <div className="font-semibold">{`${completed}/${subTaskLength}`}</div>
+
+                </div>
                 <div className="flex flex-row justify-between ml-2 mr-2">
                     <div className="bg-gray-200 text-gray-500 font-semibold rounded-full p-1 pl-2 pr-2 text-sm">{props.deadline}</div>
                     <div className="flex flex-row text-sm text-gray-500 font-semibold mt-1 ">
