fac3a02a3630c4757ac34c51578028ae6b437bcc
elevinas123 <elvinas.monstvilas@gmail.com>
2024-01-05 08:28:12 +0200

    Made daily tasks buttons unselectable

--- src/app/components/DailyTask.js
+++ src/app/components/DailyTask.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 export default function DailyTask (props) {
     return (
-            <button onClick={() => props.handleTaskClick(props.id)}   className={`text-lg font-semibold shadow-md bg-accent border-2 rounded-lg w-15vw h-12 flex justify-center items-center mt-2  transition duration-300 ease-in-out ${props.completed ? 'relative border-green-400 bg-secondary crossed-rectangle ' : 'border-neutral'} border-dashed`}>
+            <button onClick={() => props.handleTaskClick(props.id)}   className={`select-none text-lg font-semibold shadow-md bg-accent border-2 rounded-lg w-15vw h-12 flex justify-center items-center mt-2  transition duration-300 ease-in-out ${props.completed ? 'relative border-green-400 bg-secondary crossed-rectangle ' : 'border-neutral'} border-dashed`}>
                 <span className={`cursor-pointer ${props.completed ? 'active' : ''}`}>
                 {props.text}
                 </span>
